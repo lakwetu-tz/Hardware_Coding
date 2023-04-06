a2fb68b7efb7f525945604dc7ecaf5c1ea75e3c7
Lakwetu Community <52712947+lakwetu-tz@users.noreply.github.com>
2023-04-06 23:07:20 +0300

    Water Flow Sensor

---
+++ DRY_EH_Tool/mapping_code_water_flow_sensor/mapping_code_water_flow_sensor.ino
@@ -0,0 +1,20 @@
+int sensorPin = 2;
+volatile long pulse;
+unsigned long lastTime;
+int  volume;
+void setup() {
+  pinMode(sensorPin, INPUT);
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(sensorPin), increase, RISING);
+}
+void loop() {
+  volume = 2.663 * pulse;
+  int volume2=map(volume,0,1000,0,1000);
+  Serial.print("Ammount of volume");
+  Serial.print(volume2);
+  delay(300); 
+  
+}
+void increase() {
+  pulse++;
+}
