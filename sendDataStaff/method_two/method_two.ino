a494d7153f8ce7145d7613279981a7eac6921efd
Lakwetu Systems <52712947+lakwetu-tz@users.noreply.github.com>
2023-09-05 17:20:40 +0300

    Add files via upload

---
+++ sendDataStaff/method_two/method_two.ino
@@ -0,0 +1,113 @@
+#include <SoftwareSerial.h>
+SoftwareSerial sim800(10, 11);
+#include <ArduinoJson.h>
+
+ float volts;
+  int amphere;
+  int watts;
+  float temperature;
+  int humidity;
+  int pressure;
+  float smoke;
+  int load;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  sim800.begin(9600);
+  Serial.println("Initializing ...");
+
+}
+
+void ShowSerialData()
+{
+  while (sim800.available() != 0)
+    Serial.write(sim800.read());
+  delay(1000);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (sim800.available())
+  Serial.write(sim800.read());
+ 
+  sim800.println("AT");
+  delay(3000);
+ 
+  sim800.println("AT+SAPBR=3,1,\"Contype\",\"GPRS\"");
+  delay(6000);
+  ShowSerialData();
+ 
+  sim800.println("AT+SAPBR=3,1,\"APN\",\"airtelgprs.com\"");//APN
+  delay(6000);
+  ShowSerialData();
+ 
+  sim800.println("AT+SAPBR=1,1");
+  delay(6000);
+  ShowSerialData();
+ 
+  sim800.println("AT+SAPBR=2,1");
+  delay(6000);
+  ShowSerialData();
+ 
+ 
+  sim800.println("AT+HTTPINIT");
+  delay(6000);
+  ShowSerialData();
+ 
+  sim800.println("AT+HTTPPARA=\"CID\",1");
+  delay(6000);
+  ShowSerialData();
+ 
+  StaticJsonDocument<200> doc; 
+  
+  doc["volt"] = volts;
+  doc["current"] = amphere;
+  doc["energy"] = watts;
+  doc["humidity"] = humidity;
+  doc["temperature"] = temperature;
+  doc["pressure"] = pressure;
+  doc["load"] = load;
+  doc["smoke"] = smoke;
+
+  String sendtoserver;
+  serializeJson(doc, sendtoserver);
+  Serial.println(" ");  
+  serializeJsonPretty(doc, sendtoserver);
+  delay(4000);
+ 
+  sim800.println("AT+HTTPPARA=\"URL\",\"http://192.xxxxxxxxxxxxxxxxxxxxxxxx.com\""); //Server address
+  delay(4000);
+  ShowSerialData();
+ 
+  sim800.println("AT+HTTPPARA=\"CONTENT\",\"application/json\"");
+  delay(4000);
+  ShowSerialData();
+ 
+ 
+  sim800.println("AT+HTTPDATA=" + String(sendtoserver.length()) + ",100");
+  Serial.println(sendtoserver);
+  delay(6000);
+  ShowSerialData();
+ 
+  sim800.println(sendtoserver);
+  delay(6000);
+  ShowSerialData();
+ 
+  sim800.println("AT+HTTPACTION=1");
+  delay(6000);
+  ShowSerialData();
+ 
+  sim800.println("AT+HTTPREAD");
+  delay(6000);
+  ShowSerialData();
+ 
+  sim800.println("AT+HTTPTERM");
+  delay(10000);
+  ShowSerialData;
+}
+
+
---
+++ sendDataStaff/sketch_aug15a/sketch_aug15a.ino
@@ -0,0 +1,85 @@
+#include <SoftwareSerial.h>
+SoftwareSerial sim800(12, 13);
+
+struct{
+  float volts;
+  int amphere;
+  int watts;
+  float temp;
+  int humid;
+  int pressure;
+  float smoke;
+  int load;
+} values;
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
+  sim800.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+String GenerateJsonData(){
+  String aux = "{\n\t\"volt\":\"";
+  aux.concat(String(values.volts));
+  aux.concat("\",\n\t\"current\":\"");
+  aux.concat(String(values.amphere));
+  aux.concat("\",\n\t\"energy\":");
+  aux.concat(String(values.watts));
+  aux.concat("\",\n\t\"temp\":");
+  aux.concat(String(values.temp));
+  aux.concat("\",\n\t\"pressure\":");
+  aux.concat(String(values.pressure));
+  aux.concat("\",\n\t\"humidity\":");
+  aux.concat(String(values.humid));
+  aux.concat("\",\n\t\"load\":");
+  aux.concat(String(values.load));
+  aux.concat("\",\n\t\"smoke\":");
+  aux.concat(String(values.smoke));
+  
+  aux.concat("\n}");
+  return aux;
+}
+
+void TumaSasaBaba(){
+  String jsonData = GenerateJsonData();
+  String apn = "";
+  
+  sim800.println("AT");
+  delay(100);
+  sim800.println("AT+CREG?");
+  delay(500);
+   sim800.println("AT+SAPBR=3,1,Contype,GPRS");
+  delay(800);
+  sim800.println("AT+SAPBR=3,1,APN," + apn);
+  delay(800);
+  sim800.println("AT+SAPBR =1,1");
+  delay(800);
+  sim800.println("AT+SAPBR=2,1");
+  delay(800);
+  sim800.println("AT+HTTPINIT");
+  delay(800);
+  sim800.println("AT+HTTPPARA=?");
+  sim800.println("AT+HTTPPARA=CID,1");
+  delay(800);
+  sim800.println("AT+HTTPPARA=URL, http://endpoint.com/api/mainboard/values");
+  delay(2000);
+  sim800.println("AT+HTTPPARA=CONTENT, application/json");
+  sim800.print("AT+HTTPDATA=");
+  sim800.println(jsonData.length() + 2); // Add 2 for extra bytes
+  delay(100);
+  sim800.println(jsonData);
+  delay(100);
+  sim800.println("AT+HTTPACTION=1"); // Send HTTP POST request
+  delay(5000);                 
+  
+  while(sim800.available() != 0){
+    Serial.write(sim800.read());
+  }
+}
+
